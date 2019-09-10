8690c3ba94ce65d362e1c61b583b7e0f2d42ce8f
Mark Harper <markharper.sa@gmail.com>
2019-09-10 06:41:51 +0200

    WIP

--- app/src/main/java/com/entersekt/shopper/adapters/CityAdapter.kt
+++ app/src/main/java/com/entersekt/shopper/adapters/CityAdapter.kt
@@ -1,4 +1,42 @@
 package com.entersekt.shopper.adapters
 
-class CityAdapter {
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.entersekt.shopper.MallsActivity
+import com.entersekt.shopper.R
+import com.entersekt.shopsdk.data.City
+import kotlinx.android.synthetic.main.list_item_city.view.*
+
+class CityAdapter(val cityItems: List<City>?) : RecyclerView.Adapter<CityAdapter.CityViewHolder>() {
+
+    override fun getItemCount() = cityItems!!.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
+
+        return CityViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.list_item_city,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
+        holder.itemView.city_item_title.text = cityItems!![position].name
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, MallsActivity::class.java)
+            val bundle = Bundle()
+            bundle.putParcelable("city", cityItems[position])
+            intent.putExtra("bundle", bundle)
+            ContextCompat.startActivity(it.context, intent, null)
+        }
+    }
+
+    class CityViewHolder(v: View) : RecyclerView.ViewHolder(v)
 }
--- app/src/main/java/com/entersekt/shopper/adapters/MallAdapter.kt
+++ app/src/main/java/com/entersekt/shopper/adapters/MallAdapter.kt
@@ -1,4 +1,44 @@
 package com.entersekt.shopper.adapters
 
-class MallsAdapter {
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.entersekt.shopper.MallsActivity
+import com.entersekt.shopper.R
+import com.entersekt.shopper.ShopsActivity
+import com.entersekt.shopsdk.data.Mall
+import kotlinx.android.synthetic.main.list_item_city.view.*
+import kotlinx.android.synthetic.main.list_item_mall.view.*
+
+class MallAdapter(val mallItems: List<Mall>?) : RecyclerView.Adapter<MallAdapter.MallViewHolder>() {
+
+    override fun getItemCount() = mallItems!!.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MallViewHolder {
+
+        return MallViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.list_item_mall,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MallViewHolder, position: Int) {
+        holder.itemView.mall_item_title.text = mallItems!![position].name
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, ShopsActivity::class.java)
+            val bundle = Bundle()
+            bundle.putParcelable("mall", mallItems[position])
+            intent.putExtra("bundle", bundle)
+            ContextCompat.startActivity(it.context, intent, null)
+        }
+    }
+
+    class MallViewHolder(v: View) : RecyclerView.ViewHolder(v)
 }
